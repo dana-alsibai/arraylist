163ca7336299ecece3f095c3323e5f7dd67d57f3
dana-alsibai <alsibaid@sheridancollege.ca>
2020-03-05 11:54:58 -0500

    new branch

---
+++ src/danaalsibai/NewClassForItem.java
@@ -0,0 +1,32 @@
+  /*
+        Name:  [Dana Alsibai ]
+        Assignment:  [Exercise 1.1a]
+        Program: []
+        Date:  [assignment due date here]
+    
+        Description:
+        [program description in your own words]
+    */
+
+package danaalsibai;
+
+/**
+ *
+ * @author danasebai
+ */
+public class NewClassForItem {
+ private String shirtBrand;
+
+    public NewClassForItem(String shirtBrand) {
+        this.shirtBrand = shirtBrand;
+    }
+
+    public void setShirtBrand(String shirtBrand) {
+        this.shirtBrand = shirtBrand;
+    }
+
+    public String getShirtBrand() {
+        return shirtBrand;
+    }
+ 
+}
